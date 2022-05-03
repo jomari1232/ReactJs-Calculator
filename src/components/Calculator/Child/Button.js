6857fc93b0ae36febee0a837ddf161901bccf844
Jomari Barbosa <51910138+jomari1232@users.noreply.github.com>
2022-05-03 22:07:58 +0800

    first commit

---
+++ src/components/Calculator/Child/Button.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./button.scss";
+const Button = ({ title, value, input, setInput, setOutput }) => {
+  const displayNUmber = (e) => {
+    e.preventDefault();
+    let number = (input += e.target.value);
+    if (!number.includes('value')) {
+       setInput(number.replace(/^0/, ''));
+    }
+  };
+  const clear = () => {
+    setInput("0");
+    setOutput('0')
+  };
+  const deleteLastDigit = () => {
+    setInput(input.slice(0, -1)); //input.length-1
+  };
+  const percentage = () => {
+    setInput(input / 100);
+  };
+  const calculate = () => {
+    try {
+      let calculation = Function('return ' + input)
+      setOutput(calculation)
+    } catch (error) {
+      setOutput(input.slice(0, -1));
+    }
+    
+  };
+
+  return (
+    <button
+      value={value}
+      onClick={
+        value === "ac"
+          ? clear
+          : displayNUmber && value === "delete"
+          ? deleteLastDigit
+          : displayNUmber && value === "%"
+          ? percentage
+          : displayNUmber && value === "="
+          ? calculate
+          : displayNUmber
+      }>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
